c6cdcc4717bac29c6c4c4e93d076cdff4e734524
Vera Kahn <verakahn@nypl.org>
2023-08-09 11:04:40 -0400

    wip

--- lib/refinery_api.py
+++ lib/refinery_api.py
@@ -1,12 +1,15 @@
 import requests
 from requests.exceptions import JSONDecodeError, RequestException
-import time
+from datetime import datetime
 
 
 class RefineryApi:
     locations = ['schwarzman', 'lpa', 'schomburg']
     BASE_URL = "https://refinery.nypl.org/api/nypl/locations/v1.0/locations/"
     locations_cache = {}
+    # this order is based on python's datetime.weekday() which returns 0 for 
+    #   Monday and 6 for Sunday:
+    days_of_the_week = ['Monday' ,'Tuesday' ,'Wednesday' ,'Thursday' 'Friday' ,'Saturday', 'Sunday']
 
     def refresh_refinery_data(self):
         for location in self.locations:
@@ -28,7 +31,7 @@ class RefineryApi:
                 raise RefineryApiError(
                     f'Failed to parse Refinery API response: \
                         {type(e)} {e}')
-        self.locations_cache['updated_at'] = time.time()
+        self.locations_cache['updated_at'] = datetime.today()
 
     def parse_address(self, data):
         parsed_data = {}
@@ -37,9 +40,6 @@ class RefineryApi:
         parsed_data['state'] = data.get('region')
         parsed_data['postal_code'] = data.get('postal_code')
         return parsed_data
-
-    def expand_day(self, day):
-        return day.slice(0, -1) + 'day'
 
     # given an array of fields and a location code, return an dict populated
     # by those fields for that location code.
@@ -52,7 +52,8 @@ class RefineryApi:
             data['hours'] = self.locations_cache.get(location).get('hours').get('regular')
         return data
 
-    def determine_location(self, code):
+    @staticmethod
+    def determine_location(code):
         location = ''
         if code.startswith('ma'):
             location = 'schwarzman'
@@ -68,7 +69,6 @@ class RefineryApi:
     @staticmethod
     def arrange_days(days_array, today):
         # day is returned from refinery api as 'Mon.', eg
-        
         index_of_today = [day.get('day')[:-1] for day in days_array].index(today)
 
         if index_of_today == 0:
@@ -77,45 +77,33 @@ class RefineryApi:
             # reorder day objects so today is first in the array
             return days_array[index_of_today:] + days_array[0:index_of_today]
 
+    # expects time - a string representing a 24hr clock time, and day - a 
+    # datetime object. Returns a new datetime object with the same date and
+    # a new time attached
+    @staticmethod
+    def build_timestamp(time, day):
+        # extract 12 from '12:00'
+        hour = int(time.split(':')[0])
+        return day.replace(hour=hour)
+
+    # start_time and end_time are strings representing 24 hour times: '10:00'
+    # day is a datetime object
+    # index is an integer
+    @staticmethod
+    def build_hours_hash(self, start_time, end_time, day, index):
+        hours = {
+            'day': self.days_of_the_week[day.weekday()],
+            'startTime': RefineryApi.build_timestamp(start_time, day),
+            'endTime': RefineryApi.build_timestamp(end_time, day)
+        }
+        hours['today'] = index == 0
+        hours['nextBusinessDay'] = index == 1
+
+    @staticmethod
+    def build_hours_array(days_array, current_day):
+        today_weekday = current_day.weekday()
+
 
 class RefineryApiError(Exception):
     def __init__(self, message=None):
         self.message = message
-
-
-RefineryApi.arrange_days([
-    {
-        "day": "Sun.",
-        "open": "13:00",
-                "close": "17:00"
-    },
-    {
-        "day": "Mon.",
-        "open": "10:00",
-                "close": "18:00"
-    },
-    {
-        "day": "Tue.",
-        "open": "10:00",
-                "close": "20:00"
-    },
-    {
-        "day": "Wed.",
-        "open": "13:00",
-                "close": "17:00"
-    },
-    {
-        "day": "Thu.",
-        "open": "10:00",
-                "close": "18:00"
-    },
-    {
-        "day": "Fri.",
-        "open": "10:00",
-                "close": "20:00"
-    },
-    {
-        "day": "Sat.",
-        "open": "10:00",
-                "close": "20:00"
-    }], "Wed")
